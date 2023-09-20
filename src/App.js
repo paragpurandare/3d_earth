0d07472db8beb136f6226eb9e2780495eaccf04d
Parag <paragpurandare50@gmail.com>
2023-09-20 22:29:45 +0530

    created the earth component and rendered on our app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import styled from "styled-components";
+import "./App.css";
+import {Earth} from "./components/earth/Earth";
 
-const App = () => {
-  return (
-    <div>
-      Welcome to 3d world
-    </div>
-  )
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+function App() {
+  return <CanvasContainer>
+    <Canvas>
+      <Suspense fallback={null}>
+        <Earth />
+      </Suspense>
+    </Canvas>
+  </CanvasContainer>;
 }
 
-export default App
+export default App;
